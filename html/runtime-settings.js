64a0bd50eabdb842f715626b2db57ed03930e814
danielkao-31 <zell717212@gmail.com>
2026-07-28 07:45:02 +0800

    Update runtime-settings.js

--- html/runtime-settings.js
+++ html/runtime-settings.js
@@ -5,8 +5,8 @@
  * 前端不得放置試算表 ID、Drive 資料夾 ID 或管理密碼。
  */
 window.APP_RUNTIME_CONFIG = Object.freeze({
-  gasWebAppUrl: 'https://script.google.com/macros/s/AKfycbzohEAuUplqyu0iZOwu2RxdnwwFf6MJptlYHHnw5TvDyUgLswS_VVVBPQRedWO1hTw/exec',
+  gasWebAppUrl: 'https://script.google.com/macros/s/AKfycbwP3NSPi1CXfUeaMkLj_bvae3kQgCrP0-8QYfrA4EmWAemTqMIBJxrjGzcBG2uDjPOI/exec',
   adminGasWebAppUrl: 'https://script.google.com/macros/s/AKfycbzDn10QPub6PXuJ8AAPJtEwCP0n48_iqv3HjEsni2Ou8pXDAURWtxXWMQaUkB3C6WmT/exec',
   expectedApiContractVersion: '1.1.0',
-  assetVersion: '20260727-v01321-fix12-rc4'
+  assetVersion: '20260728-btest-rc5'
 });
