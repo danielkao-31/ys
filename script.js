b0008c991c1eb756f3baea2e57d39a0f43d1a53a
danielkao-31 <zell717212@gmail.com>
2026-07-28 09:33:20 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * GitHub Pages 前端連接 GAS JSON API。
+ * 前端不得放置試算表 ID、Drive 資料夾 ID 或管理密碼。
+ */
+window.APP_RUNTIME_CONFIG = Object.freeze({
+  gasWebAppUrl: 'https://script.google.com/macros/s/AKfycbwP3NSPi1CXfUeaMkLj_bvae3kQgCrP0-8QYfrA4EmWAemTqMIBJxrjGzcBG2uDjPOI/exec',
+  adminGasWebAppUrl: '',
+  expectedApiContractVersion: '1.1.0',
+  assetVersion: '20260728-v01321-fix12-rc7'
+});
